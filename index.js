1873f763ef468432fdda10d766cedf7edf9c3139
arkkab <arkkab@gmail.com>
2020-08-04 16:40:28 +0200

    add scroll feature

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
 
     document.addEventListener('wheel', (event)=> {
-        console.log(event.pageY);
+        if(isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(()=>{
+            isThrottled=false;
+        }, 1000);
+
+        const direction = event.deltaY > 0 ? 1 : -1;
+        if(direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length -1;
+            if (isLastSection) return;
+        } else if(direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if(firstSection) return;
+        }
+        currentSectionIndex = currentSectionIndex + direction;
+        console.log(currentSectionIndex)
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
     })
 })
